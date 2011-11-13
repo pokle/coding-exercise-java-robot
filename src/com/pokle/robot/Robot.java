74ff26947b51e6347dc4f488da0c3f8b7b9d8907
Tushar Pokle <tushar.pokle@gmail.com>
2011-11-13 19:37:15 +1100

    Initial empty project

---
+++ src/com/pokle/robot/Robot.java
@@ -0,0 +1,11 @@
+package com.pokle.robot;
+
+/**
+ * Created by IntelliJ IDEA.
+ * User: tushar
+ * Date: 13/11/11
+ * Time: 7:33 PM
+ * To change this template use File | Settings | File Templates.
+ */
+public class Robot {
+}
---
+++ test/com/pokle/robot/RobotTest.java
@@ -0,0 +1,12 @@
+package com.pokle.robot;
+
+import org.junit.Test;
+
+public class RobotTest {
+
+    @Test
+    public void shouldNotMoveUnlessPlaced() {
+
+    }
+
+}
